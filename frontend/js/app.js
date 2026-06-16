83fa8e9576d7ec799e7dfe9825121266483d0362
Urme <148221893+urmeo@users.noreply.github.com>
2026-06-17 05:36:45 +0600

    escape html

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -663,9 +663,12 @@ function showToast(message, type = 'info') {
 // Utilities
 function esc(text) {
     if (!text) return '';
-    const div = document.createElement('div');
-    div.textContent = text;
-    return div.innerHTML;
+    return String(text)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
 }
 
 // Global function exports for onclick handlers
